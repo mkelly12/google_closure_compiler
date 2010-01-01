0d5d2048239f782eac3031b0158ecffeed0fcc3c
Matt <matt@zurb.com>
2010-01-01 09:58:45 -0800

    Add helper to include closure base.js when calcdeps.py is not used.

---
+++ lib/app/helpers/google_closure_helper.rb
@@ -0,0 +1,13 @@
+module GoogleClosureHelper
+  
+  def closure_include_tag(file_path)
+    out = ''
+    unless ActionController::Base.perform_caching
+      closure_base_path = File.join(GoogleClosureCompiler.closure_library_path, 'goog')
+      out << "<script src='#{File.join('/javascripts', closure_base_path, 'base.js')}' type='text/javascript'></script>"
+    end
+    out << javascript_include_tag(file_path, :cache => "cache/closure/#{file_path.delete('.js')}")
+    out
+  end
+
+end
