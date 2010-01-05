dc3f5f7ae23c459905b89f60c6375a0f57cf2d53
Matt <matt@zurb.com>
2010-01-05 14:40:20 -0800

    Include google events scripts in dev environment

--- lib/app/helpers/google_closure_helper.rb
+++ lib/app/helpers/google_closure_helper.rb
@@ -5,6 +5,7 @@ module GoogleClosureHelper
     unless ActionController::Base.perform_caching
       closure_base_path = File.join(GoogleClosureCompiler.closure_library_path, 'goog')
       out << "<script src='#{File.join('/javascripts', closure_base_path, 'base.js')}' type='text/javascript'></script>"
+      out << "<script type='text/javascript'>goog.require('goog.events');</script>"
     end
     out << javascript_include_tag(file_path, :cache => "cache/closure/#{file_path.delete('.js')}")
     out
